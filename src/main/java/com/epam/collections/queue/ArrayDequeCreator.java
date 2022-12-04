ab53a68a3de4fd1a43f41401f5bd575ae287d49a
Sano <sanasar.hambardzumyan@gmail.com>
2022-12-04 21:35:42 +0400

    Changes

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -1,10 +1,34 @@
 package com.epam.collections.queue;
 
-import java.util.ArrayDeque;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        deque.add(firstQueue.remove());
+        deque.add(firstQueue.remove());
+        deque.add(secondQueue.remove());
+        deque.add(secondQueue.remove());
+
+        int i = 0;
+        while (!firstQueue.isEmpty() || !secondQueue.isEmpty()){
+            if(i % 2 == 0){
+                firstQueue.add(deque.removeLast());
+                deque.add(firstQueue.remove());
+                deque.add(firstQueue.remove());
+            }
+            else {
+                secondQueue.add(deque.removeLast());
+                deque.add(secondQueue.remove());
+                deque.add(secondQueue.remove());
+            }
+            i++;
+        }
+        return deque;
     }
+
+
+
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,31 @@
 package com.epam.collections.queue;
 
-import java.util.List;
+import java.util.*;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+
+        Deque<Integer> queue = new ArrayDeque<>();
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 1; i <= numberOfDishes; i++) {
+            queue.addLast(i);
+        }
+
+        int i = 1;
+
+        while (!queue.isEmpty()){
+            if(i % everyDishNumberToEat == 0){
+                list.add(queue.removeFirst());
+                i = 0;
+            }
+            else
+            {
+                queue.addLast(queue.removeFirst());
+            }
+            i++;
+        }
+       return list;
     }
+
 }
